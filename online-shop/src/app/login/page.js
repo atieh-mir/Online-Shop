3cc3482277f521c9260aa32d8e6b09fe7358c112
atieh-mir <atiehmir17@gmail.com>
2024-02-15 22:57:40 +0330

    create login page

---
+++ online-shop/src/app/login/page.js
@@ -0,0 +1,14 @@
+
+
+
+export default function Login(){
+
+
+    return(
+        <div className="bg-rose-50 min-h-screen flex items-center justify-center">
+            <div >
+                hi
+            </div>
+        </div>
+    )
+}
