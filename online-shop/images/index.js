d99fd97a2fb06a37a6608f46417d577f94e8ba1b
atieh-mir <atiehmir17@gmail.com>
2024-02-16 12:10:42 +0330

    initial commit

---
+++ online-shop/images/index.js
@@ -0,0 +1,17 @@
+import user from './1.png';
+import cart from './2.png';
+import logo from './3.png';
+import loginImg from './4.png';
+import cross from './cross.png';
+import menu from './menu.png';
+
+
+
+export {
+    user,
+    cart,
+    logo,
+    loginImg,
+    cross,
+    menu
+}
---
+++ online-shop/src/app/componets/index.js
@@ -0,0 +1,8 @@
+
+
+export * from "./Header";
+export * from "./Navbar";
+export * from "./products/Headphones";
+export * from "./products/Laptop";
+export * from "./products/Phone";
+export * from "./products/ListProducts";
